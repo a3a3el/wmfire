df146c1858e125172d8dd216ca820b7d2a13360e
Jeremy Sowden <jeremy@azazel.net>
2023-06-20 22:50:10 +0100

    Use `AC_CONFIG_HEADERS`
    It makes the output of make easier to read.

--- src/session.c
+++ src/session.c
@@ -18,6 +18,11 @@
 /* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 /******************************************/
 
+#ifndef CONFIG_H_INCLUDED
+#include "config.h"
+#define CONFIG_H_INCLUDED
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
